bc414296b39e83511fd14c523e26c3ee5828d308
Vadim1906 <vadivi2009@yandex.ru>
2018-04-14 14:35:02 +0300

    Initial Commit

---
+++ DP/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DP
+//
+//  Created by Вадим Иванов on 14.04.2018.
+//  Copyright © 2018 Вадим Иванов. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
